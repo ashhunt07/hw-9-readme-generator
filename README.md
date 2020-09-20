a30d851da76f2ce95fde17f62c487db13ad4f13b
Ashley Hunt <ashhunt07@gmail.com>
2020-09-20 02:25:29 -0500

    Successful Node test, filling in additional content

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # hw-9-readme-generator
+
+# ${response.title}
+    
+## Table of Contents
+    
+
+- One
+- Two
+- One
+- Two
+- One
+- Two
+- One
+- Two
+- One
+- Two
--- index.js
+++ index.js
@@ -3,94 +3,118 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
 
+// Questions Required: title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-// Questions Needed: title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
-// array of questions for user
-const questions = [
-
-    {
-        type: "input",
-        name: "badge",
-        message: "Insert badge code: ",
-    },
-    {
-        type: "input",
-        name: "title",
-        message: "What is the title of the project? ",
-    },
-    {
-        type: "input",
-        name: "title",
-        message: "Describe your project: ",
-    },
-    {
-        type: "input",
-        name: "photo",
-        message: "Project Screenshot: ",
-    },
-    {
-        type: "input",
-        name: "title",
-        message: "What are the installtion instructions? ",
-    },
-    {
-        type: "input",
-        name: "title",
-        message: "How should this be used? ",
-    },
-    {
-        type: "checkbox",
-        name: "license",
-        message: "Choose a license: ",
-        choices: [
-            "MIT license",
-            "GLP License",
-            "ISC License",
-        ]
-    },
-    {
-        type: "input",
-        name: "technology",
-        message: "What Techologies were utilized? ",
-        choices: [
-            "HTML",
-            "CSS",
-            "JavaScript",
-            "JQuerry",
-            "jSON",
-        ]
-    },
-    {
-        type: "input",
-        name: "test",
-        message: "Do a test! ",
-    },
-    {
-        type: "input",
-        name: "author",
-        message: "Who contributed to the project? ",
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "Link to GitHub: ",
-    },
-    {
-        type: "input",
-        name: "contribution",
-        message: "What are the guidelines for contribution? ",
-    },
-
-];
+// layout for readme information
+function writeToFile(questions) {
+    return `
+    
+    # ${response.title}
+    
+    ### Table of Contents
+    
 
 
 
-// function to write README file
-function writeToFile(fileName, data) {}
+
+
+    `
+
+}
 
 // function to initialize program
-function init() {
+async function init() {
+    try {
+        // array of questions for user
+        const questions = await inquirer.prompt([
+
+            {
+                type: "input",
+                name: "badge",
+                message: "Insert badge code: ",
+            },
+            {
+                type: "input",
+                name: "title",
+                message: "What is the title of the project? ",
+            },
+            {
+                type: "input",
+                name: "author",
+                message: "Who contributed to the project? ",
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "Link to GitHub: ",
+            },
+            {
+                type: "input",
+                name: "title",
+                message: "Describe your project: ",
+            },
+            {
+                type: "input",
+                name: "photo",
+                message: "Project Screenshot: ",
+            },
+            {
+                type: "input",
+                name: "title",
+                message: "What are the installtion instructions? ",
+            },
+            {
+                type: "input",
+                name: "code",
+                message: "What is the node command? ",
+            },
+            {
+                type: "input",
+                name: "title",
+                message: "How should this be used? ",
+            },
+            {
+                type: "checkbox",
+                name: "license",
+                message: "Choose a license: ",
+                choices: [
+                    "MIT license",
+                    "GLP License",
+                    "ISC License",
+                ]
+            },
+            {
+                type: "input",
+                name: "technology",
+                message: "What Techologies were utilized? ",
+                choices: [
+                    "HTML",
+                    "CSS",
+                    "JavaScript",
+                    "JQuerry",
+                    "jSON",
+                ]
+            },
+            {
+                type: "input",
+                name: "test",
+                message: "Do a test! ",
+            },
+            {
+                type: "input",
+                name: "contribution",
+                message: "What are the guidelines for contribution? ",
+            },
+
+        ]);
+
+        const readMe = await writeToFile(questions);
+        await fs.writeFileSync("README.md", readme);
+        console.log("You've successfully wrote to README.md");
+    } catch (error) {
+        console.error(error);
+
+    }
 
 }
 
